74de7a7656b13accc1f4cd1e45168239e661f25e
eam 01Dec2019 <eammonjobrien@gmail.com>
2020-05-03 22:08:21 +0100

    ADDING LINKS 2

--- app.R
+++ app.R
@@ -91,7 +91,7 @@ ui <- fluidPage(theme = shinytheme("journal"), #https://www.rdocumentation.org/p
                                 
                                 
                                 actionButton(inputId='ab1', label="R Shiny ",   icon = icon("th"),   
-                                             onclick ="window.open(https://raw.githubusercontent.com/eamonn2014/sd-specification/master/app.R', '_blank')"), 
+                                             onclick ="window.open('https://raw.githubusercontent.com/eamonn2014/sd-specification/master/app.R', '_blank')"), 
                                 actionButton(inputId='ab1', label="R code",   icon = icon("th"),   
                                              onclick ="window.open('https://raw.githubusercontent.com/eamonn2014/sd-specification/master/sd%20specification.R', '_blank')"),  
                                 actionButton("resample", "Simulate a new sample"),
